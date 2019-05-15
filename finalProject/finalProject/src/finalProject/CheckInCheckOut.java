0c1fe4ecada783e864e5452c0a21ebdaae99f503
SunTzuu <33640516+SunTzuu@users.noreply.github.com>
2019-05-15 15:54:24 -0700

    Add files via upload
    The CheckInCheckOut class has been updated with 4 Lists that takes Guest objects.

--- finalProject/finalProject/src/finalProject/CheckInCheckOut.java
+++ finalProject/finalProject/src/finalProject/CheckInCheckOut.java
@@ -4,67 +4,65 @@ import java.util.ArrayList;
 
 public class CheckInCheckOut {
 
-    private ArrayList<Observer> reservationList;
-    private ArrayList<Observer> waitingList;
-    //This reference is used to track those who made reservations have acutalliy checkedIn
-    private ArrayList<Observer> checkedIn;
-    private ArrayList<Observer> checkedOut;
+    private ArrayList<Guest> reservationList;
+    private ArrayList<Guest> waitingList;
+    private ArrayList<Guest> checkedIn;
+    private ArrayList<Guest> checkedOut;
     /**
      * @return the reservationList
      */
-    public ArrayList<Observer> getReservationList() {
+    public ArrayList<Guest> getReservationList() {
         return reservationList;
     }
 
     /**
      * @param reservationList the reservationList to set
      */
-    public void setReservationList(ArrayList<Observer> reservationList) {
+    public void setReservationList(ArrayList<Guest> reservationList) {
         this.reservationList = reservationList;
     }
 
     /**
      * @return the waitingList
      */
-    public ArrayList<Observer> getWaitingList() {
+    public ArrayList<Guest> getWaitingList() {
         return waitingList;
     }
 
     /**
      * @param waitingList the waitingList to set
      */
-    public void setWaitingList(ArrayList<Observer> waitingList) {
+    public void setWaitingList(ArrayList<Guest> waitingList) {
         this.waitingList = waitingList;
     }
 
     /**
      * @return the checkedIn
      */
-    public ArrayList<Observer> getCheckedIn() {
+    public ArrayList<Guest> getCheckedIn() {
         return checkedIn;
     }
 
     /**
      * @param checkedIn the checkedIn to set
      */
-    public void setCheckedIn(ArrayList<Observer> checkedIn) {
+    public void setCheckedIn(ArrayList<Guest> checkedIn) {
         this.checkedIn = checkedIn;
     }
 
     /**
      * @return the checkedOut
      */
-    public ArrayList<Observer> getCheckedOut() {
+    public ArrayList<Guest> getCheckedOut() {
         return checkedOut;
     }
 
     /**
      * @param checkedOut the checkedOut to set
      */
-    public void setCheckedOut(ArrayList<Observer> checkedOut) {
+    public void setCheckedOut(ArrayList<Guest> checkedOut) {
         this.checkedOut = checkedOut;
     }
-        
 
-    
+
 }
